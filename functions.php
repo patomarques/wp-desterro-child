72745d20b4b39c441550ce40a170672b524ada2e
Pato Marques <patoxmarques@gmail.com>
2021-06-17 23:28:34 -0300

    rename project name references

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php 
 
-wp_enqueue_style( 'lulacortes-css', get_theme_file_uri() . '/assets/scss/lulacortes.css');
-wp_enqueue_script( 'lulacortes-js', get_theme_file_uri() . '/assets/js/lulacortes.js', array('jquery'), '1.0', true );
+wp_enqueue_style( 'desterro-css', get_theme_file_uri() . '/assets/scss/desterro.css');
+wp_enqueue_script( 'desterro-js', get_theme_file_uri() . '/assets/js/desterro.js', array('jquery'), '1.0', true );
 
 function hide_admin_bar(){ return false; }
 add_filter( 'show_admin_bar', 'hide_admin_bar' );
